57273a8f01f1bbd53dcc57d8edb497d50f8523fc
jhojoo <jaysys@gmail.com>
2023-03-07 19:07:08 +0900

    Update

--- app.py
+++ app.py
@@ -2,12 +2,39 @@ from flask import Flask, render_template, request
 from pyzbar.pyzbar import decode
 from PIL import Image
 import os
+import qrcode
+import base64
+import codecs
+from io import BytesIO
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('read.html')
+
+@app.route('/gen', methods=['GET', 'POST'])
+def gen():
+    if request.method == 'POST':
+        input_text = request.form.get('input_text')
+        qr = qrcode.QRCode(version=1, box_size=10, border=5)
+        qr.add_data(input_text)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img_buffer = BytesIO()
+        img.save(img_buffer, format='PNG')
+        img_buffer.seek(0)
+        img_data = codecs.decode(base64.b64encode(img_buffer.getvalue()), 'ascii')
+
+        # Save image to file
+        with open('qr_gen.png', 'wb') as f:
+            f.write(img_buffer.getvalue())
+
+        return render_template('gen.html', input_text=input_text, img_data=img_data)
+    else:
+        return render_template('gen.html')
+
+
 
 @app.route('/scan', methods=['POST'])
 def scan():
